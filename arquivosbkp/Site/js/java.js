fad88feb6420f154bd14f4a831c6870403675bc9
Lucas Rodrigues Vilarinho <38113511+lucasvlr@users.noreply.github.com>
2026-06-08 17:05:41 -0300

    atualizacao

--- arquivosbkp/Site/js/java.js
+++
@@ -1,9 +0,0 @@
-console.log("Dashboard ONG carregado");
-
-// contador
-let rescued = document.getElementById("rescuedCount");
-
-setInterval(() => {
-    let current = parseInt(rescued.innerText);
-    if (current < 140) rescued.innerText = current + 1;
-}, 5000);
